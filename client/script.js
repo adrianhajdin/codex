d73a84550dccc8bfffeb853d94ec2fb5522bd12c
adrianhajdin <adrianhajdin00@gmail.com>
2022-12-20 21:05:38 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://codex-g91f.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
